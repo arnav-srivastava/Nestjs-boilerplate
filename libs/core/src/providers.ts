f6ca3ebe70d8c3ea76ce7a6d65e04c3dfe4f1689
Arnav Srivastava <80662620+arnav-srivastava@users.noreply.github.com>
2023-06-30 15:52:42 +0530

    Initialize feature/cli-scaffolding

---
+++ libs/core/src/providers.ts
@@ -0,0 +1,35 @@
+import {
+  ListCommands,
+  InitApplicationSetup,
+  CreateModule,
+  CreateTransformer,
+} from './console';
+import { HttpExplorer } from './http';
+import { BaseValidator, ExistsConstraint } from './validator';
+import { IsUniqueConstraint } from './validator/decorators/isUnique';
+import { IsValueFromConfigConstraint } from './validator/decorators/isValueFromConfig';
+
+const providers = [
+  // commands
+  ListCommands,
+  InitApplicationSetup,
+  CreateModule,
+  CreateTransformer,
+
+  // custom base validator
+  BaseValidator,
+
+  // http providers
+  HttpExplorer,
+
+  // custom validator decorators
+  IsUniqueConstraint,
+  ExistsConstraint,
+  IsValueFromConfigConstraint,
+];
+
+const getProviders = function (): any {
+  return providers;
+};
+
+export { getProviders };
